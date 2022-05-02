916b820559995eaeaacc620ff5b76b610491659d
Trp_yusef <240560@vutbr.cz>
2022-05-02 11:58:07 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
-
+def recrusive_nth_fibo(n):
+    if n == 0:
+        return n
+    elif n == 1:
+        return n
+    else:
+        return recrusive_nth_fibo(n-1) + recrusive_nth_fibo(n-2)
 def main():
-    pass
+    print(recrusive_nth_fibo(5))
 
 
 if __name__ == "__main__":
